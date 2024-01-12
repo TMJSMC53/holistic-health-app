a4ee8456a2c745427da9b6ccf0313c83db277cc8
Triola Michelle Jackson <triolajackson@gmail.com>
2024-01-11 21:28:27 -0600

    Added functionality for DELETE request

--- client/src/components/FluidIntakeDeleteForm.tsx
+++ client/src/components/FluidIntakeDeleteForm.tsx
@@ -1,18 +1,14 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 
-const FluidIntakeDeleteForm = () => {
-  const [fluidAmount, setFluidAmount] = useState('');
-  const [fluidType, setFluidType] = useState('');
-
+const FluidIntakeDeleteForm = ({ ...props }) => {
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      const response = await fetch('/api/fluid', {
+      const response = await fetch(`/api/fluid/${props.item._id}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ fluidType: fluidType, amount: fluidAmount }),
       });
 
       const fluidIntakeDoc = await response.json();
@@ -22,45 +18,26 @@ const FluidIntakeDeleteForm = () => {
     }
   }
 
-  function handleFluidType(event: ChangeEvent<HTMLInputElement>) {
-    setFluidType(event.target.value);
-  }
-  function handleFluidAmount(event: ChangeEvent<HTMLInputElement>) {
-    setFluidAmount(event.target.value);
-  }
   return (
     <>
-      <div className="mt-24 mb-12">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="fluidType">Choose a fluid type from this list:</label>
-          <input
-            className="input input-bordered input-sm w-2/12 max-w-xs ml-2 mr-1.5"
-            list="fluids"
-            id="fluidType"
-            name="fluidType"
-            value={fluidType}
-            onChange={handleFluidType}
-            type="string"
-          />
-          <datalist id="fluids">
-            <option value="Water"></option>
-            <option value="Coffee"></option>
-            <option value="Tea"></option>
-            <option value="Carbonated Water"></option>
-            <option value="Juice"></option>
-          </datalist>
-          <input
-            className="input input-bordered input-sm w-2/12 max-w-xs mr-1.5"
-            value={fluidAmount}
-            type="number"
-            onChange={handleFluidAmount}
-            placeholder="Amount"
-          />
-          <button className="btn btn-sm btn-accent text-blue">
-            Enter Amount
-          </button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </button>
+      </form>
     </>
   );
 };
