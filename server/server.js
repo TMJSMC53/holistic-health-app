82d3959beacd43f64694ee9198cc360c0a8ad77e
Triola Michelle Jackson <triolajackson@gmail.com>
2024-01-29 14:01:18 -0600

    Intstalled and added cookie parser to server.js file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //e.g server.js
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import ViteExpress from 'vite-express';
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5151;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use('/', router);
