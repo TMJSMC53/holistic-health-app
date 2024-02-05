365384d65379db14b4c81f0f2b592bfcfa257bc1
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-04 20:07:18 -0600

    created SetUser and UserState to have them globally

--- client/src/main.d.ts
+++ client/src/main.d.ts
@@ -2,4 +2,8 @@ export type User = {
   username: string;
   firstName: string;
   lastName: string;
-}
+};
+
+export type SetUser = React.Dispatch<React.SetStateAction<UserState>>;
+
+export type UserState = User | null | undefined;
