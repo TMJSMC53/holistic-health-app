c5b5d6ebbbcb64a1c13a2d581c9e791a54d3c653
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-04 20:08:49 -0600

    added functionality to the logout button

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,5 +1,21 @@
-const Logout = () => {
-  return <div>Logout</div>;
+import { SetUser } from '../main.d';
+import { useNavigate } from 'react-router-dom';
+type LogoutProps = {
+  setUser: SetUser;
+};
+const Logout = ({ setUser }: LogoutProps) => {
+  const navigate = useNavigate();
+  return (
+    <button
+      className="btn btn-outline btn-secondary"
+      onClick={() => {
+        navigate('/');
+        setUser(null);
+      }}
+    >
+      Logout
+    </button>
+  );
 };
 
 export default Logout;
