6c1bf4ab0b7c6391eea75e6bfc431d02bb482212
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-04 20:12:38 -0600

    created the logic that shows a dashboard btn if the user goes to the homepage
    connected the logout btn

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-// import { User } from '../main.d';
+import { UserState, SetUser } from '../main.d';
+import Logout from './Logout';
 
-// type NavbarProps = {
-//   user: User | null;
-//   setUser: React.Dispatch<React.SetStateAction<User | null>>;
-// };
+type NavbarProps = {
+  user: UserState;
+  setUser: SetUser;
+};
 
-// const Navbar = ({ user, setUser }: NavbarProps) => {
-const Navbar = () => {
+const Navbar = ({ user, setUser }: NavbarProps) => {
+  // const Navbar = () => {
   return (
     <>
       <div className="drawer">
@@ -39,9 +40,15 @@ const Navbar = () => {
             <div className="flex-none hidden lg:block">
               <ul className="menu menu-horizontal">
                 {/* Navbar menu content here */}
+
                 <li>
-                  <a>Home</a>
+                  <a href="/">Home</a>
                 </li>
+                {user ? (
+                  <li>
+                    <a href="/dashboard">Dashboard</a>
+                  </li>
+                ) : null}
                 <li>
                   <a>About</a>
                 </li>
@@ -49,7 +56,7 @@ const Navbar = () => {
                   <a>Docs</a>
                 </li>
                 <li>
-                  <a className="btn btn-sm btn-secondary">Logout</a>
+                  <Logout setUser={setUser} />
                 </li>
               </ul>
             </div>
@@ -74,69 +81,11 @@ const Navbar = () => {
               <a>Docs</a>
             </li>
             <li>
-              <a className="btn btn-outline btn-secondary">Logout</a>
+              <Logout setUser={setUser} />
             </li>
           </ul>
         </div>
       </div>
-
-      {/* <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Home</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Docs</a>
-              </li>
-              <div className="">
-                <a className="btn btn-outline btn-secondary">Logout</a>
-              </div>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">HH</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Home</a>
-            </li>
-
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Docs</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn btn-outline btn-secondary">Logout</a>
-        </div>
-      </div> */}
     </>
   );
 };
