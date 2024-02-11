9d047841bdade9067de5184c8a8c6620092316c8
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-10 22:23:48 -0600

    added isLoggedIn functionality for users when they are logged out

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,19 +1,52 @@
 import { SetUser } from '../main.d';
+import { MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 type LogoutProps = {
   setUser: SetUser;
+  isLoggedIn: boolean;
 };
-const Logout = ({ setUser }: LogoutProps) => {
+const Logout = ({ setUser, isLoggedIn }: LogoutProps) => {
   const navigate = useNavigate();
+
+  async function handleSubmit(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    try {
+      const response = await fetch(`/logout`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log({ response });
+      if (response.ok) {
+        // Registration successful
+
+        const logoutResponse = await response.json();
+        alert(logoutResponse.message);
+
+        // setUser(loginResponse.user);
+        setUser(null);
+        navigate(`/`);
+      } else {
+        // Registration failed
+        const errorMessage = await response.text();
+        alert(`Logout failed: ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
   return (
     <button
-      className="btn btn-outline btn-secondary"
-      onClick={() => {
-        navigate('/');
-        setUser(null);
-      }}
+      className="btn btn-xs sm:btn-sm md:btn-md bg-primary-500"
+      onClick={handleSubmit}
+      // onClick={() => {
+
+      //   // navigate('/');
+      //   // setUser(null);
+      // }}
     >
-      Logout
+      {isLoggedIn ? 'Logout' : 'Login'}
     </button>
   );
 };
