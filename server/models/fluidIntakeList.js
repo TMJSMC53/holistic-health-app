95c98d1b035232103987adc21e6311d6a5f4bf3f
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-11 13:10:13 -0600

    added the user_id to the model schema

--- server/models/fluidIntakeList.js
+++ server/models/fluidIntakeList.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
 const fluidIntakeSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
   fluidType: {
     type: String,
     required: true,
   },
-
   amount: {
     type: Number,
     required: true,
