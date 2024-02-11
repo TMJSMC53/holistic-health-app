a3ef8e84c31f876e86d5122c8a7e5e78921f90cf
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-11 16:22:13 -0600

    added the user_id to the schema

--- server/models/waterIntakeGoal.js
+++ server/models/waterIntakeGoal.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const waterIntakeGoalSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
   amount: {
     type: Number,
     required: true,
