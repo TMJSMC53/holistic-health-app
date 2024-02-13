67d569f5cf3056370081e173611eda59ab4c5c5d
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-12 20:50:44 -0600

    changed the hero for when the user is logged in and they click on the home link

--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -5,13 +5,37 @@ import { SetUser } from '../../main.d';
 
 type HomepageProps = {
   setUser: SetUser;
+  isLoggedIn: boolean;
+  // handleLogout: () => void;
 };
 
-const Homepage = ({ setUser }: HomepageProps) => {
+const Homepage = ({ setUser, isLoggedIn }: HomepageProps) => {
   return (
     <div>
       <main>
-        <HomepageAuthForm setUser={setUser} />
+        {isLoggedIn ? (
+          <div
+            className="hero min-h-screen bg-primary-500
+      "
+          >
+            <div className="hero-content flex-col lg:flex-row-reverse py-14">
+              <div className="text-center lg:text-left">
+                <h1 className="text-5xl text-accents-100 font-bold text-center font-playfair">
+                  Welcome Back to Holistic Health
+                </h1>
+              </div>
+              <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <img
+                  className="rounded"
+                  src="/images/deniz-altindas-unsplash.jpg"
+                  alt="Hero image"
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <HomepageAuthForm setUser={setUser} />
+        )}
       </main>
       <ProductFeatures />
       <footer>
