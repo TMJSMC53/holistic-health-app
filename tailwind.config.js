eeaacebe054d9734962c39b8c0524cd3affc13e3
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-24 13:03:48 -0600

    added custom box shadow code

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,10 @@ export default {
     borderRadius: generateBorderRadius(),
     fontSize: generateFontSize(),
     extend: {
+      boxShadow: {
+        custom:
+          'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+      },
       colors: {
         primary: {
           400: '#42b883',
