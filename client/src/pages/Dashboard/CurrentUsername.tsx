39b95b2437103f59075c2d2034c3e67a43117e86
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-29 15:22:17 -0600

    added font poppins and font-weight medium to h3

--- client/src/pages/Dashboard/CurrentUsername.tsx
+++ client/src/pages/Dashboard/CurrentUsername.tsx
@@ -4,7 +4,9 @@ type CurrentUserNameProps = {
 
 const CurrentUsername = ({ firstName }: CurrentUserNameProps) => {
   return (
-    <h3 className="ml-4 text-18 font-poppins">Welcome Back, {firstName}</h3>
+    <h3 className="ml-4 text-18 text-primary-600 font-poppins font-medium">
+      Welcome Back, {firstName}
+    </h3>
   );
 };
 
