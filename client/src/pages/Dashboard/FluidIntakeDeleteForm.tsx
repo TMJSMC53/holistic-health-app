d573b3ad3d9e9807033b60fdc4434d53477e7300
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-29 20:45:23 -0600

    changed the delete icon size on device in FluidIntakeDeleteForm component

--- client/src/pages/Dashboard/FluidIntakeDeleteForm.tsx
+++ client/src/pages/Dashboard/FluidIntakeDeleteForm.tsx
@@ -28,7 +28,7 @@ const FluidIntakeDeleteForm = ({ fluid }: { fluid: Fluid }) => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className="w-4 h-4 md:w-6 md:h-6"
           >
             <path
               strokeLinecap="round"
