781a1fd55cd2ab161a614b8ea67e54291efa58b9
Triola Michelle Jackson <triolajackson@gmail.com>
2024-02-29 21:21:27 -0600

    removed the min-h-screen class in HomepageAuthForm component

--- client/src/pages/Homepage/HomepageAuthForm.tsx
+++ client/src/pages/Homepage/HomepageAuthForm.tsx
@@ -27,7 +27,7 @@ const HomepageAuthForm = ({ setUser }: HomepageAuthFormProps) => {
 
   return (
     <>
-      <div className="min-h-screen md:min-h-96 bg-accents-300">
+      <div className="bg-accents-300">
         <div className="flex flex-col lg:flex-row-reverse lg:justify-center lg:items-center px-4 py-14">
           <div className="text-center lg:text-left lg:pl-8">
             <h1 className="text-26 mb-2 md:mb-6 md:text-40 lg:text-56 text-primary-600 font-bold text-center font-playfair">
