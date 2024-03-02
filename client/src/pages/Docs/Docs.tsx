a90bf189cb221fd227bdc6cf0f78d64cd9493e9d
Triola Michelle Jackson <triolajackson@gmail.com>
2024-03-02 14:00:07 -0600

    created Docs component and added coming soon text

---
+++ client/src/pages/Docs/Docs.tsx
@@ -0,0 +1,9 @@
+const Docs = () => {
+  return (
+    <div className="my-20 mx-8">
+      <h2 className="text-36 text-primary-700 font-playfair">COMING SOON...</h2>
+    </div>
+  );
+};
+
+export default Docs;
