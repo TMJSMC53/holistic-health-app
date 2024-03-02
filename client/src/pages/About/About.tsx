f36c92f952dd901a6b620c014cbe747a23f63c2e
Triola Michelle Jackson <triolajackson@gmail.com>
2024-03-02 14:01:52 -0600

    created About component page

---
+++ client/src/pages/About/About.tsx
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <div>
+      <h2>Holistic Health</h2>
+
+      <p>
+        We know how important water is for our overall health, and with the
+        daily comings and goings of life, we don't consume as much water as we
+        should on a daily basis. Our app aims to make such an important aspect
+        of our daily life as easy as possible with this app. Whether you're on
+        the go, on vacation, or at home vibing, you are able to track your
+        fluids and water. At the end of the day, you will have a clear and easy
+        way of knowing that you are consuming the right amount of fluids each
+        day in one place.
+      </p>
+    </div>
+  );
+};
+
+export default About;
