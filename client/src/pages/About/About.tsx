f4381cb698157f366b410cdd02e06e6534e351f2
Triola Michelle Jackson <triolajackson@gmail.com>
2024-03-30 12:57:11 -0600

    added styling to the about page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,13 +1,17 @@
 const About = () => {
   return (
-    <div>
-      <p>
-        At Holistic Health, we're dedicated to empowering individuals to lead
-        healthier lives. Our app is a comprehensive tool designed to help you
-        track various aspects of your well-being, including fluid and water
-        intake, sleep patterns, meditation practices, and daily notes. More than
-        just a tracker, our app is a personalized health companion that keeps
-        you motivated and focused on your wellness journey.
+    <div className="text-20 mt-12 mx-10 text-justify font-poppins">
+      <p className="mb-4">
+        At
+        <span className="text-24 font-cedarville text-primary-700 ml-2">
+          Holistic Health
+        </span>
+        , we're dedicated to empowering individuals to lead healthier lives. Our
+        app is a comprehensive tool designed to help you track various aspects
+        of your well-being, including fluid and water intake, sleep patterns,
+        meditation practices, and daily notes. More than just a tracker, our app
+        is a personalized health companion that keeps you motivated and focused
+        on your wellness journey.
       </p>
       <p>
         Whether you're looking to stay hydrated, improve your sleep quality, or
