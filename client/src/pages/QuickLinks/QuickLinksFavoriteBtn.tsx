ccee8b28da8cac30919fa789330ce33e2a23422a
Triola Michelle Jackson <triolajackson@gmail.com>
2024-12-09 21:08:20 -0600

    -fix: update SVG attributes to use camelCase for React compatibility in QuickLinksFavoriteBtn component.

--- client/src/pages/QuickLinks/QuickLinksFavoriteBtn.tsx
+++ client/src/pages/QuickLinks/QuickLinksFavoriteBtn.tsx
@@ -23,13 +23,13 @@ const QuickLinksFavoriteBtn = ({
           height="20px"
           stroke="#ff0000"
         >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             stroke="#ff0000"
-            stroke-width="3.072"
+            strokeWidth="3.072"
           >
             <title>hearts</title>
             <path d="M27.267 11.489c0.21 6.687-10.266 11.384-11.25 15.056-1.075-4.011-11.060-8.078-11.283-15.056-0.214-6.701 8.047-8.155 11.283-2.55 3.316-5.743 11.043-4.039 11.25 2.55z"></path>
@@ -49,13 +49,13 @@ const QuickLinksFavoriteBtn = ({
           height="20px"
           stroke="#ffffff"
         >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             stroke="#ff0000"
-            stroke-width="3.072"
+            strokeWidth="3.072"
           >
             <title>hearts</title>
             <path d="M27.267 11.489c0.21 6.687-10.266 11.384-11.25 15.056-1.075-4.011-11.060-8.078-11.283-15.056-0.214-6.701 8.047-8.155 11.283-2.55 3.316-5.743 11.043-4.039 11.25 2.55z"></path>
