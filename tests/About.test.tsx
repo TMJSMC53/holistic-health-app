5051e8d633c768d289578d551ee95b5e43bd6402
Triola Michelle Jackson <triolajackson@gmail.com>
2025-01-18 17:52:37 -0600

    Refined tests for the About.tsx component

--- tests/About.test.tsx
+++ tests/About.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import About from '../client/src/pages/About/About';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 
 describe('About', () => {
@@ -28,16 +28,23 @@ describe('About', () => {
   });
 
   it('should take the user to the previous page when back button is clicked', async () => {
+    // GIVEN the user is on the previous dashboard page, and then goes to the about page
     render(
-      <MemoryRouter initialEntries={['/about']}>
+      <MemoryRouter initialEntries={['/dashboard', '/about']}>
+        <Routes>
+          <Route path="/dashboard" element={'Dashboard'} />
+          <Route path="/about" element={'About'} />
+        </Routes>
         <About />
       </MemoryRouter>
     );
-
+    // WHEN user clicks the back button
     const user = userEvent.setup();
-    const backButton = screen.getByTestId('back-button');
+    const backButton = screen.getByText('Back');
+
     await user.click(backButton);
 
-    expect(window.location.pathname).not.toBe('/about');
+    // THEN the user is navigated to the Dashboard
+    expect(screen.queryByText('Dashboard')).not.toBeNull();
   });
 });
